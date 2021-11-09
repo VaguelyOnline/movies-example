ad7307c8425e0b9be9d8ddab2b0d41f06562575c
davidasmall <davidsmall727@protonmail.com>
2021-11-09 17:05:39 +0000

    Create UserSeeder.php

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->createFirstUserRootAdmin();
+        $this->createSecondUserNoPermissions();
+    }
+
+    private function createFirstUserRootAdmin()
+    {
+        
+    }
+
+    private function createSecondUserNoPermissions()
+    {
+        User::create([
+            'name' => 'Loreto',
+            'email' => 'loreto@email.com',
+            "password" => Hash::make('password'),
+        ]);
+    }
+}
